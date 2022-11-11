cf1827fed5128912b0a958374eca897a97ab2eda
sleb <scott.g.lebaron@gmail.com>
2022-11-11 03:38:47 +0000

    first working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{command, Parser};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// domain to update
+    domain: String,
+
+    /// account token
+    token: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let url = format!(
+        "https://duckdns.org/update?domains={}&token={}&verbose=true",
+        &cli.domain, &cli.token
+    );
+
+    match ureq::get(&url).call() {
+        Ok(response) => {
+            println!(
+                "{}",
+                response.into_string().unwrap_or_default().escape_debug()
+            );
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e.to_string());
+        }
+    }
 }
